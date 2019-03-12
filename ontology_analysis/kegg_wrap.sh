b2845ea4acebf87abe5c6d313c371ad272ca7124
Jacob <jacob.cooper@utah.edu>
2019-03-12 17:46:23 -0600

    Working on exporatory analysis. Have a plot made for the most represented categories in each clade

---
+++ ontology_analysis/kegg_wrap.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+./multi_dics.py /Users/Jacob/corsair/ontology_analysis/converted_hit_lists/Hsap_converted_hits.txt
+./multi_dics.py /Users/Jacob/corsair/ontology_analysis/converted_hit_lists/Mluc_converted_hits.txt
+./multi_dics.py /Users/Jacob/corsair/ontology_analysis/converted_hit_lists/Pman_converted_hits.txt
+./multi_dics.py /Users/Jacob/corsair/ontology_analysis/converted_hit_lists/Mmus_converted_hits.txt
+./multi_dics.py /Users/Jacob/corsair/ontology_analysis/converted_hit_lists/Btau_converted_hits.txt
+./multi_dics.py /Users/Jacob/corsair/ontology_analysis/converted_hit_lists/Clup_converted_hits.txt
--- ontology_analysis/multi_dics.py
+++ ontology_analysis/multi_dics.py
@@ -5,6 +5,25 @@ from copy import deepcopy
 
 hit_list = sys.argv[1]
 kegg_db = '/Users/Jacob/corsair/ontology_analysis/hsa00001_formated.txt'
+outfile = '/Users/Jacob/corsair/ontology_analysis/kegg_counting/' + hit_list.split('/')[-1].split('_')[0] + '_kegg_counting.csv'
+
+def count_gene(gene_list, kegg_dict):
+    count_dict = {}
+    for key in kegg_dict:
+        count_dict[key] = []
+        for gene in gene_list:
+            if gene in kegg_dict[key]:
+                count_dict[key].append(gene)
+    
+    return count_dict
+
+
+def kegg_ratio(gene_list, kegg_dict, count_dict):
+    ratio_dict = {}
+    for key in kegg_dict:
+        kegg_r = (len(count_dict[key]) / len(gene_list)) / len(kegg_dict[key])
+        ratio_dict[key] = kegg_r
+    return ratio_dict
 
 A = {}
 B = {}
@@ -51,27 +70,6 @@ with open(hit_list, 'r') as f:
         line = line.strip()
         gene_list.append(line)
 
-
-
-def count_gene(gene_list, kegg_dict):
-    count_dict = {}
-    for key in kegg_dict:
-        count_dict[key] = []
-        for gene in gene_list:
-            if gene in kegg_dict[key]:
-                count_dict[key].append(gene)
-    
-    return count_dict
-
-
-def kegg_ratio(gene_list, kegg_dict, count_dict):
-    ratio_dict = {}
-    for key in kegg_dict:
-        kegg_r = (len(count_dict[key]) / len(gene_list)) / len(kegg_dict[key])
-        ratio_dict[key] = kegg_r
-    return ratio_dict
-
-
 count_A = count_gene(gene_list, A)
 count_B = count_gene(gene_list, B)
 count_C = count_gene(gene_list, C)
@@ -80,19 +78,26 @@ ratio_A = kegg_ratio(gene_list, A, count_A)
 ratio_B = kegg_ratio(gene_list, B, count_B)
 ratio_C = kegg_ratio(gene_list, C, count_C)
 
+## write to the output file
+with open(outfile, 'w') as f:    
+    f.write("Pathway,Ratio,Hits,Total\n")
+    for key in ratio_B:
+        f.write("{},{},{},{}".format(key, ratio_B[key], len(count_B[key]), len(B[key])) + '\n')
+    for key in ratio_C:
+        f.write("{},{},{},{}".format(key, ratio_C[key], len(count_C[key]), len(C[key])) + '\n')
 
 ### print
-print('######### A ##########')
-for key in A:
-    print("{} | genes:{}/{} | kegg ratio:{}".format(key, len(count_A[key]), len(A[key]), ratio_A[key]))
-print('\n')
+# print('######### A ##########')
+# for key in A:
+#     print("{} | genes:{}/{} | kegg ratio:{}".format(key, len(count_A[key]), len(A[key]), ratio_A[key]))
+# print('\n')
 
-print('######### B ##########')
-for key in B:
-    print("{} | genes:{}/{} | kegg ratio:{}".format(key, len(count_B[key]), len(B[key]), ratio_B[key]))
-print('\n')
+# print('######### B ##########')
+# for key in B:
+#     print("{} | genes:{}/{} | kegg ratio:{}".format(key, len(count_B[key]), len(B[key]), ratio_B[key]))
+# print('\n')
 
-print('######### C ##########')
-for key in C:
-    print("{} | genes:{}/{} | kegg ratio:{}".format(key, len(count_C[key]), len(C[key]), ratio_C[key]))
-print('\n')
+# print('######### C ##########')
+# for key in C:
+#     print("{} | genes:{}/{} | kegg ratio:{}".format(key, len(count_C[key]), len(C[key]), ratio_C[key]))
+# print('\n')
